27639ba456e39116e1e6e384167039e5c7fda481
FrahOrru <francesca.orru@marconirovereto.it>
2020-07-20 15:42:19 +0200

    colors and font

--- js/carousel.js
+++ js/carousel.js
@@ -9,18 +9,21 @@ function Carousel(option){
         <img src="${item.image}"></img>
         </div>
         <div>
-        <p> titolo ${item.title}</p>
+        <p> titolo: ${item.title}</p>
         </div>
         </div>`
     }
     document.getElementById(a.container).innerHTML = 
-    `<div class="header"> 
+    `
+     <div class="header"> 
      <h4 class="title">${a.title}</h4>
      <h5 class="subtitile"> ${a.subtitle} </h5> 
      </div>
+     <button> < </button>
      <div id="items">
      ${arrayElem.map(SingleItem).join('')}
-     </div>`
+     </div>
+     <button> > </button>`
 
     
      
