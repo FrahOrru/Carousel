56c36d016e4db1e7b0d0cc1c4bb3c038ad880c27
FrahOrru <francesca.orru@marconirovereto.it>
2020-07-20 12:11:27 +0200

    first display of tite and  img of single item

--- js/carousel.js
+++ js/carousel.js
@@ -2,23 +2,46 @@ function Carousel(option){
     var a = option;
     var d = document.getElementById(a.container);
 
-    var tag = document.createElement("div");
-    tag.setAttribute('id', "item")
-    d.appendChild(tag);
-
-    var e = document.getElementById("item");
+    //title and subtitle carousel
     var title = document.createElement("h4");
     var text = document.createTextNode(a.title);
-    e.appendChild(title);
+    d.appendChild(title);
     title.appendChild(text);
 
     var subtitle = document.createElement("h5");
     var textsub = document.createTextNode(a.subtitle);
-    e.appendChild(subtitle);
+    d.appendChild(subtitle);
     subtitle.appendChild(textsub);
-    
-  
-    
-    
 
+    //single item
+    var arrayElem = a.fetchCards();
+ 
+    arrayElem.forEach(element => {
+
+        var tag = document.createElement("div");
+        tag.setAttribute('id', "item")
+        d.appendChild(tag);
+
+        var e = document.getElementById("item");
+
+        var picture = document.createElement("img")
+        picture.setAttribute('src', element.image)
+        e.appendChild(picture);
+
+        // var content = document.createElement("div");
+        // tag.setAttribute('id', "content")
+        // e.appendChild(content);
+
+        // var f = document.getElementById("content");
+
+        var title = document.createTextNode(element.title);
+        e.appendChild(title);
+
+    });
+   
+  
+        
+  
+     
 }
+
